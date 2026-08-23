54c5385cf077d314a2d7e0fc444efdf087edd500
midoalhashmi-crypto <midoalhashmi@icloud.com>
2026-08-24 01:54:47 +0300

    Create app_features.dart

---
+++ lib/core/config/app_features.dart
@@ -0,0 +1,14 @@
+class AppFeatures {
+  static const bool showSearch = true;
+  static const bool showCategories = true;
+  static const bool showAnnouncements = true;
+  static const bool showFavorites = true;
+  static const bool showLive = true;
+  static const bool showAds = false;
+  static const bool showSports = true;
+
+  static const bool liveScoreEnabled = false;
+  static const bool notificationsEnabled = true;
+  static const bool backgroundPlaybackEnabled = false;
+  static const bool liveChatEnabled = false;
+}
